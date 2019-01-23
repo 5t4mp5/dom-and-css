828ad838e4a8e2e667fac67ee371c61e55bf2059
unknown <5t4mp5@gmail.com>
2019-01-23 16:22:22 -0500

    buttons and squares generated in js

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const colors = ['red', 'green', 'blue'];
+//TODO DRY OUT CLEAN UP REFACTOR
+
+const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange', 'pink'];
+colors.forEach(color => {
+    const square = document.createElement('div');
+    square.setAttribute('class', 'squares');
+    document.querySelector('#squares').appendChild(square);
+});
+
 const squareList = [...document.querySelectorAll('.squares')];
 const showAll = document.querySelector('#showAll');
 const hideAll = document.querySelector('#hideAll');
@@ -6,28 +14,56 @@ const switchRed = document.querySelector('#switchRed');
 const switchGreen = document.querySelector('#switchGreen');
 const switchBlue = document.querySelector('#switchBlue');
 
+colors.forEach((color, idx) => {
+    squareList[idx].setAttribute('style', `background-color: ${color}`);
+    squareList[idx].setAttribute('id', color);
+    const button = document.createElement('button');
+    button.id = `${color}Switch`;
+    button.setAttribute('class', 'colorButtons');
+    button.setAttribute('switch', 'off');
+    button.innerHTML = `HIDE ${color.toUpperCase()}`;
+    button.addEventListener('click', function(){
+        switchDisplayOne(color, this);
+    });
+    document.querySelector('#colorButtons').appendChild(button);
+});
+
+
+const colorButtons = [...document.querySelectorAll('.colorButtons')];
+console.log(colorButtons);
+
 function switchDisplayAll(display){
     const opposite = {none: '', '': 'none'};
     hideAll.style.display = display;
     showAll.style.display = opposite[display];
 
-    switchDisplayOne('red', switchRed);
-    switchDisplayOne('blue', switchBlue);
-    switchDisplayOne('green', switchGreen);
+    colors.forEach(color =>{
+        const status = display === '' ? 'on' : 'off';
+        const button = colorButtons.find(button => button.id === `${color}Switch`);
+        switchDisplayOne(color, button, status);
+    });
 }
 
-function switchDisplayOne(color, button){
+function switchDisplayOne(color, button, forceStatus){
     const square = squareList.find(square => square.getAttribute('id') === color);
-    
-    if (button.getAttribute('switch') === 'off'){
+    if(forceStatus === 'on'){
+        square.style.display = '';
+        button.setAttribute('switch', 'off');
+        button.innerHTML = `HIDE ${color.toUpperCase()}`;
+    }else if(forceStatus === 'off'){
         square.style.display = 'none';
         button.setAttribute('switch', 'on');
         button.innerHTML = `SHOW ${color.toUpperCase()}`;
     }else{
-        console.log('else');
-        square.style.display = '';
-        button.setAttribute('switch', 'off');
-        button.innerHTML = `HIDE ${color.toUpperCase()}`;
+        if (button.getAttribute('switch') === 'off'){
+            square.style.display = 'none';
+            button.setAttribute('switch', 'on');
+            button.innerHTML = `SHOW ${color.toUpperCase()}`;
+        }else{
+            square.style.display = '';
+            button.setAttribute('switch', 'off');
+            button.innerHTML = `HIDE ${color.toUpperCase()}`;
+        }
     }
 
     if (squareList.every(square => square.style.display === 'none')){
@@ -36,13 +72,11 @@ function switchDisplayOne(color, button){
     }else if(squareList.every(square => square.style.display === '')){
         showAll.style.display = 'none';
         hideAll.style.display = '';
-    }
+    }else if(squareList.some(square => square.style.display === '')){
+        showAll.style.display = '';
+        hideAll.style.display = '';
+    };
 }
-
-colors.forEach((color, idx) => {
-    squareList[idx].setAttribute('style', `background-color: ${color}`);
-    squareList[idx].setAttribute('id', color);
-});
 
 hideAll.addEventListener('click', function(){
     switchDisplayAll('none');
@@ -50,16 +84,4 @@ hideAll.addEventListener('click', function(){
 
 showAll.addEventListener('click', function(){
     switchDisplayAll('');
-});
-
-switchRed.addEventListener('click', function(){
-    switchDisplayOne('red', this);
-});
-
-switchGreen.addEventListener('click', function(){
-    switchDisplayOne('green', this);
-});
-
-switchBlue.addEventListener('click', function(){
-    switchDisplayOne('blue', this);
 });
